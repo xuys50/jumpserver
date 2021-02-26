4e433cfbc974d9f709922d1d09f3a7aa2090c5e1
xinwen <coderWen@126.com>
2021-02-26 16:55:14 +0800

    优化

--- apps/perms/utils/asset/user_permission.py
+++ apps/perms/utils/asset/user_permission.py
@@ -324,7 +324,7 @@ class UserGrantedUtilsBase:
 
     def __init__(self, user, asset_perm_ids=None):
         self.user = user
-        self._asset_perm_ids = asset_perm_ids
+        self._asset_perm_ids = set(asset_perm_ids)
 
     @lazyproperty
     def asset_perm_ids(self) -> set:
